99c3ffed692807d040e7ece27816732e8ee9b15c
rinisha <rinisharinup@gmail.com>
2026-06-07 01:40:48 +0530

    first_tailwind_templates_commit

--- studentapp/forms.py
+++ studentapp/forms.py
@@ -1,19 +1,86 @@
+# from django import forms
+# from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.models import User
+# from studentapp.models import * 
+
+# class SignupForm(UserCreationForm):
+#     class Meta:
+#         model=User
+#         fields=["first_name","last_name","username","email","password1","password2"]
+
+# class signinForm(forms.Form):
+#     username = forms.CharField(max_length=100,widget=forms.TextInput(attrs={"class":"form-control"}))
+#     password = forms.CharField(max_length=100,widget=forms.PasswordInput(attrs={"class":"form-control"}))
+
+# class StudentForm(forms.ModelForm):
+#     class Meta:
+#         model=StudentModel
+#         fields='__all__'
+    
+# from django import forms
+# from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.models import User
+# from studentapp.models import *
+
+# class SignupForm(UserCreationForm):
+#     class Meta:
+#         model = User  
+#         fields = ["first_name","last_name","username", "email", "password1", "password2"]
+
+
+# class signinForm(forms.Form):
+#     username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={"class": "form-control"}))
+#     password = forms.CharField(max_length=100, widget=forms.PasswordInput(attrs={"class": "form-control"}))
+
+
+# class StudentForm(forms.ModelForm):
+#     class Meta:
+#         model = StudentModel
+#         fields = '__all__'
+
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+        
+#         for field in self.fields.values():
+#             field.widget.attrs.update({
+#                 'class': 'w-full px-4 py-2 rounded-xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500'
+#             })
+
+
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from studentapp.models import * 
+from django.contrib.auth.forms import UserCreationForm
+from .models import StudentModel  # Ningalude model import cheyyuka
 
 class SignupForm(UserCreationForm):
     class Meta:
-        model=User
-        fields=["username","email","password1","password2"]
+        model = User
+        fields = ["first_name", "last_name", "username", "email", "password1", "password2"]
+
+    # Ella fields-num tailwind classes apply cheyyan __init__ add cheyyuka
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({
+                'class': 'w-full px-4 py-2 rounded-xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 text-black'
+            })
 
 class signinForm(forms.Form):
-    username = forms.CharField(max_length=100,widget=forms.TextInput(attrs={"class":"form-control"}))
-    password = forms.CharField(max_length=100,widget=forms.PasswordInput(attrs={"class":"form-control"}))
+    username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
+        'class': 'w-full px-4 py-2 rounded-xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 text-black'
+    }))
+    password = forms.CharField(max_length=100, widget=forms.PasswordInput(attrs={
+        'class': 'w-full px-4 py-2 rounded-xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 text-black'
+    }))
 
 class StudentForm(forms.ModelForm):
     class Meta:
-        model=StudentModel
-        fields='__all__'
-    
+        model = StudentModel
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({
+                'class': 'w-full px-4 py-2 rounded-xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 text-black'
+            })
--- studentproject/settings.py
+++ studentproject/settings.py
@@ -119,3 +119,9 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
